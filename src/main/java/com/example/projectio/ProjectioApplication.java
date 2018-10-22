604cc088b22dbdb6f5cbd11555e51c9b41c8cde7
DominikKossinski <37476821+DominikKossinski@users.noreply.github.com>
2018-10-22 16:45:03 +0200

    First Commit

---
+++ src/main/java/com/example/projectio/ProjectioApplication.java
@@ -0,0 +1,12 @@
+package com.example.projectio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectioApplication.class, args);
+    }
+}
