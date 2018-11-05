ad587a2364ef230a34edd4cfafc6d9235a89769c
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2018-11-05 16:47:57 +0100

    Done: CapitalizeRestController

--- src/main/java/com/example/projectio/Translator.java
+++ src/main/java/com/example/projectio/Translator.java
@@ -7,4 +7,14 @@ public class Translator {
     }
 
     public static String toUpperCase(String text) {return text.toUpperCase(); }
+
+    public static String toCapitalize(String text)
+    {
+        StringBuilder result = new StringBuilder(text.length());
+        String words[] = text.split("\\ ");
+        for (int i = 0; i < words.length; i++)
+            result.append(Character.toUpperCase(words[i].charAt(0))).append(words[i].substring(1)).append(" ");
+
+        return result.toString();
+    }
 }
