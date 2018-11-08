cfae796613c27018ec28774efda4488b5f41f8e5
DominikKossinski <37476821+DominikKossinski@users.noreply.github.com>
2018-11-08 13:39:11 +0100

    Done: myShortcutsExpand and MyShortcutRestController

--- src/main/java/com/example/projectio/MyRestController.java
+++ src/main/java/com/example/projectio/MyRestController.java
@@ -3,9 +3,17 @@ package com.example.projectio;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Przykładowy RestController
+ */
 @RestController
 public class MyRestController {
 
+    /**
+     * Przykładowa metoda
+     *
+     * @return - Hello World
+     */
     @RequestMapping("/")
     public String getHello() {
         return "Hello world";
