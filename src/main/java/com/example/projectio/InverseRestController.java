3595971985576ec144cbc3a7a8ddbd675ef10eec
Lukeram <smokczysko4@gmail.com>
2018-11-08 17:09:53 +0100

    Dodano komentarze i inne takie bajery

--- src/main/java/com/example/projectio/InverseRestController.java
+++ src/main/java/com/example/projectio/InverseRestController.java
@@ -4,8 +4,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RestController odpowiadający za obracanie podanego tekstu z zachowaniem
+ * wielkości liter na odpowiednich pozycjach
+ *
+ * @author Mariusz
+ */
 @RestController
 public class InverseRestController {
+    /**
+     * Metoda klasy InverseRestController pozwalająca na obsługę żądania obrócenia
+     * tekstu z zachowaniem wielkości liter na odpowiednich pozycjach
+     *
+     * Przykładowy URl:
+     * http:\\localhost:8080/api/inverse?text=MariuSz
+     *
+     * @param text - (String) tekst podany przez użytkownika
+     * @return (String) tekst po obróceniu oraz uporządkowaniu wielkości
+     *                  liter
+     * @see Translator#toInverse(String)
+     */
     @GetMapping("/api/inverse")
     public String getTextToUpperCase(@RequestParam(name = "text") String text) {
         return Translator.toInverse(text);
