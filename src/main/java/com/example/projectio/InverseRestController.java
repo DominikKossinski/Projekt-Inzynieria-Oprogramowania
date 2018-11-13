e73f9ca1d6d57a9f3b65ec5f418f5b53f7c738c7
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2018-11-13 14:02:43 +0100

    Dodano logger obslugujacy INFO

--- src/main/java/com/example/projectio/InverseRestController.java
+++ src/main/java/com/example/projectio/InverseRestController.java
@@ -4,10 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class InverseRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(InverseRestController.class);
+
     @GetMapping("/api/inverse")
     public String getTextToUpperCase(@RequestParam(name = "text") String text) {
+
+        logger.info("Rozpoczynam translację metodą inverse");
+
         return Translator.toInverse(text);
     }
 }
