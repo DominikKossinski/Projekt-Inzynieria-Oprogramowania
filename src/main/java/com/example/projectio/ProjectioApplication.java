d69000f2c2dc35e46f7a246bb07a93e87dc8b993
DominikKossinski <37476821+DominikKossinski@users.noreply.github.com>
2018-12-12 12:40:47 +0100

    Data base correction

--- src/main/java/com/example/projectio/ProjectioApplication.java
+++ src/main/java/com/example/projectio/ProjectioApplication.java
@@ -10,7 +10,6 @@ public class ProjectioApplication {
 
     private static JdbcTemplate jdbcTemplate = null;
 
-
     public static void main(String[] args) {
         SpringApplication.run(ProjectioApplication.class, args);
     }
@@ -19,9 +18,9 @@ public class ProjectioApplication {
         if (jdbcTemplate == null) {
             DriverManagerDataSource dataSource = new DriverManagerDataSource();
             dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test");
-            dataSource.setUsername("root");
-            dataSource.setPassword("password");
+            dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+            dataSource.setUsername(System.getenv("dbUserName"));
+            dataSource.setPassword(System.getenv("dbPassword"));
             jdbcTemplate = new JdbcTemplate(dataSource);
         }
         return jdbcTemplate;
